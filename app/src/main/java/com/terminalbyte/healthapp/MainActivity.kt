90f02643c362a75b941882831728aacbd3491b15
Kailaash Jeevan <kailaash80@gmail.com>
2019-08-10 16:51:35 +0530

    Login Page entry Done.

--- app/src/main/java/com/terminalbyte/healthapp/MainActivity.kt
+++ app/src/main/java/com/terminalbyte/healthapp/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.terminalbyte.healthapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.terminalbyte.healthapp.R.id.loginButton
+import kotlinx.android.synthetic.main.login_or_signup.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var loginB: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_layout)
+        setContentView(R.layout.login_or_signup)//present screen
         setSupportActionBar(findViewById(R.id.action_bar))
+        loginB= findViewById(R.id.loginButton) as Button//get the login button
+        loginB?.setOnClickListener{//if the Login button is clicked
+            //Intent of the login page
+            val int= Intent(this, loginActivity::class.java)
+            startActivity(int)//login page opens
+        }
+
+
     }
 }
